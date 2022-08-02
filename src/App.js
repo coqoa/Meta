6923c529ec9f4106da63f4789299bda6997c68fc
ChoiByeongMin <coqoa28@gmail.com>
2022-08-02 18:09:37 +0900

    123

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meta Clone Coding.
+          Meta Clone Coding..
         </p>
         <a
           className="App-link"
